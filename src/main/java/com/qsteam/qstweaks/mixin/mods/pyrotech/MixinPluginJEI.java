20ca546654ebb70a63d3482f34d43caf4b3d8695
Bronit <suslinv037@gmail.com>
2026-05-17 20:41:13 +0300

    update Pyrotech tweaks

--- src/main/java/com/qsteam/qstweaks/mixin/mods/pyrotech/MixinPluginJEI.java
+++ src/main/java/com/qsteam/qstweaks/mixin/mods/pyrotech/MixinPluginJEI.java
@@ -1,7 +1,9 @@
 package com.qsteam.qstweaks.mixin.mods.pyrotech;
 
 import com.codetaylor.mc.pyrotech.modules.tech.basic.ModuleTechBasic.Blocks;
+import com.codetaylor.mc.pyrotech.modules.tech.basic.block.BlockDryingRack.EnumType;
 import com.codetaylor.mc.pyrotech.modules.tech.basic.plugin.jei.PluginJEI;
+import com.codetaylor.mc.pyrotech.modules.tech.basic.plugin.jei.category.JEIRecipeCategoryDryingRack;
 import com.codetaylor.mc.pyrotech.modules.tech.basic.plugin.jei.category.JEIRecipeCategoryWorktable;
 import com.qsteam.qstweaks.config.QSModIntegrationConfig;
 import mezz.jei.api.IModRegistry;
@@ -10,6 +12,7 @@ import mezz.jei.api.recipe.IRecipeCategoryRegistration;
 import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -28,25 +31,32 @@ public class MixinPluginJEI {
             )
     )
     private void redirectRegisterCategories(IRecipeCategoryRegistration registry, IRecipeCategory[] categories) {
-        if (QSModIntegrationConfig.PYROTECH.workbenchCategoryTweak) {
-            IRecipeCategory[] filtered = Arrays.stream(categories)
-                    .filter(category -> !(category instanceof JEIRecipeCategoryWorktable))
-                    .toArray(IRecipeCategory[]::new);
+        IRecipeCategory[] filtered = Arrays.stream(categories)
+                .filter(this::qstweaks$shouldKeepCategory)
+                .toArray(IRecipeCategory[]::new);
 
-            registry.addRecipeCategories(filtered);
-        } else {
-            registry.addRecipeCategories(categories);
-        }
+        registry.addRecipeCategories(filtered);
+    }
+
+    @Unique
+    private boolean qstweaks$shouldKeepCategory(IRecipeCategory category) {
+        return !(
+                (QSModIntegrationConfig.PYROTECH.workbenchCategoryTweak && category instanceof JEIRecipeCategoryWorktable)
+                || (QSModIntegrationConfig.PYROTECH.mergeDryingCategory && category instanceof JEIRecipeCategoryDryingRack)
+        );
     }
 
     @Inject(
             method = "register",
-            at = @At("HEAD")
+            at = @At("TAIL")
     )
     private void qstweaks$register(IModRegistry registry, CallbackInfo cb) {
         if (QSModIntegrationConfig.PYROTECH.workbenchCategoryTweak) {
             registry.addRecipeCatalyst(new ItemStack(Blocks.WORKTABLE), VanillaRecipeCategoryUid.CRAFTING);
             registry.addRecipeCatalyst(new ItemStack(Blocks.WORKTABLE_STONE), VanillaRecipeCategoryUid.CRAFTING);
+        }
+        if (QSModIntegrationConfig.PYROTECH.mergeDryingCategory) {
+            registry.addRecipeCatalyst(new ItemStack(Blocks.DRYING_RACK, 1, EnumType.NORMAL.getMeta()), "pyrotech.crude.drying");
         }
     }
 
